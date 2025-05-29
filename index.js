26630e23982848ca08d87707fbca7a0bfd02bbc8
Ben Thornton <smenjas@users.noreply.github.com>
2025-05-28 19:54:17 -0700

    Restore answers after reloading the page.

--- index.js
+++ index.js
@@ -4,12 +4,21 @@ function parseId(id) {
     return [parseInt(name.slice(1)), parseInt(value)];
 }
 
-const answers = [];
+const answers = JSON.parse(localStorage.getItem('answers')) || [];
+
+// Restore answers after reloading the page.
+for (const index in answers) {
+    const question = parseInt(index) + 1;
+    const answer = answers[index];
+    document.getElementById(`q${question}-${answer}`).checked = true;
+}
+
 const radios = document.querySelectorAll('input[type=radio]');
 
 for (const radio of radios) {
     radio.addEventListener('click', (event) => {
         const [question, value] = parseId(event.target.id);
         answers[question - 1] = value;
+        localStorage.setItem('answers', JSON.stringify(answers));
     });
 }
